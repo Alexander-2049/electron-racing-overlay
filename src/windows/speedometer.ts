0a98baea7209fad806d0281f94f245f17ca6e2aa
Alexander2049 <a.alexander.2049@gmail.com>
2024-08-29 09:48:55 +0300

    Added properties to an overlay window

--- src/windows/speedometer.ts
+++ src/windows/speedometer.ts
@@ -1,16 +1,40 @@
 import { BrowserWindow } from "electron";
 
-export const createSpeedometer = (url: string): void => {
+export const createSpeedometer = (url: string) => {
   // Create the browser window.
   const speedometerWindow = new BrowserWindow({
-    height: 130,
-    width: 600,
+    height: 43,
+    width: 474,
     frame: false,
     transparent: true,
     alwaysOnTop: true,
+    hasShadow: false,
+    minimizable: false,
+    fullscreenable: false,
+    skipTaskbar: true,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
   // and load the index.html of the app.
   speedometerWindow.loadURL(url + "#speedometer");
 
-  speedometerWindow.setAlwaysOnTop(true);
+  speedometerWindow.setAlwaysOnTop(true, "screen-saver");
+  speedometerWindow.setVisibleOnAllWorkspaces(true); // Ensures it's visible across all workspaces
+  speedometerWindow.setFullScreenable(false);
+
+  speedometerWindow.on("minimize", () => {
+    speedometerWindow.restore();
+  });
+
+  speedometerWindow.on("system-context-menu", (event) => {
+    event.preventDefault();
+  });
+
+  speedometerWindow.on("close", (event) => {
+    event.preventDefault();
+  });
+
+  return speedometerWindow;
 };
