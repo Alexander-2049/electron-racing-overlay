b38a7727c8a9da758b38ead0c0c085f9edfc4d6c
Alexander2049 <a.alexander.2049@gmail.com>
2024-09-03 09:32:40 +0300

    Replaced websocket server with an IPC

--- forge.config.ts
+++ forge.config.ts
@@ -14,7 +14,7 @@ import { rendererConfig } from "./webpack.renderer.config";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    extraResource: ["./assets/irsdk-wss.exe"],
+    extraResource: ["./assets/irsdk-ipc.exe"],
   },
   rebuildConfig: {},
   makers: [
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 import path from "path";
 
-export const PORT_WSS = "4004";
 export const IS_DEV = process.env.NODE_ENV === "development";
 
 export const RESOURCES_PATH = IS_DEV
