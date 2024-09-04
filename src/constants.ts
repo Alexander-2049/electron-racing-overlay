45ec8e1f9e25dd023ef178f6d3394654bc39f8a5
Alexander-2049 <a.alexander.2049@gmail.com>
2024-09-05 01:03:30 +0300

    Added path to mods in constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
+import { app } from "electron";
 import path from "path";
 
 export const IS_DEV = process.env.NODE_ENV === "development";
@@ -5,3 +6,5 @@ export const IS_DEV = process.env.NODE_ENV === "development";
 export const RESOURCES_PATH = IS_DEV
   ? path.join(__dirname, "../../", "assets")
   : path.join(process.resourcesPath);
+
+export const MODS_PATH = path.join(app.getPath("userData"), "mods");
