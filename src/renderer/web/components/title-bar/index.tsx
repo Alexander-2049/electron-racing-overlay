e8bc1bcc165ee2826ee882f9e0ec8a14890986bd
Alexander-2049 <a.alexander.2049@gmail.com>
2024-09-18 21:53:01 +0300

    Changed color scheme and added icons to title-bar buttons

--- src/renderer/web/components/title-bar/index.tsx
+++ src/renderer/web/components/title-bar/index.tsx
@@ -1,3 +1,5 @@
+import CloseIcon from "../icons/close";
+import MinimizeIcon from "../icons/minimize";
 import "./title-bar.css";
 
 const TitleBar = () => {
@@ -6,18 +8,20 @@ const TitleBar = () => {
       <div className="title-bar-left"></div>
       <div className="title-bar-right">
         <button
+          className="title-bar-button title-bar-minimize-btn"
           onClick={() => {
             window.titleBar.sendMessage("minimize");
           }}
         >
-          -
+          <MinimizeIcon width="20px" height={"20px"} fill="white" />
         </button>
         <button
+          className="title-bar-button title-bar-close-btn"
           onClick={() => {
             window.titleBar.sendMessage("close");
           }}
         >
-          X
+          <CloseIcon width="20px" height={"20px"} fill="white" />
         </button>
       </div>
     </div>
